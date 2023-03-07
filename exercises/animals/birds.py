f8431663a5f05b83e49bfd33223f0aca9f79232c
Hannes Gustafsson <hannes.gustafsson@kemi.uu.se>
2023-03-07 14:18:58 +0100

    start-up, exercises a,b and c

---
+++ exercises/animals/birds.py
@@ -0,0 +1,13 @@
+"""
+Module contaning the birds class definition
+"""
+
+class Birds:
+    
+    def __init__(self, )
+        self.members = ['Sparrow', 'Crow', 'Robin', 'Duck', 'Eagle', 'Hawk']
+
+    def printMembers(self)
+        print(self.members)
+
+
---
+++ exercises/animals/mammals.py
@@ -0,0 +1,11 @@
+"""
+Contain the mammals class definition.
+"""
+
+class Mammals:
+    def __init__(self):
+        self.members = ['Cat', 'Dog', 'Bear', 'Wolf', 'Sheep', 
+                        'Pangolin', 'Panda']
+
+    def printMembers(self):
+        print(self.members)
