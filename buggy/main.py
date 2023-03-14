211021f508e79d6bc1700b0f3619aa45e106cff0
Hannes Gustafsson <hannes.gustafsson@kemi.uu.se>
2023-03-14 20:20:44 +0100

    add/remove debugger line

--- buggy/main.py
+++ buggy/main.py
@@ -4,7 +4,7 @@ def main():
     print("Add the values of the dice")
     print("It's really that easy")
     print("What are you doing with your life.")
-    import ipdb; ipdb.set_trace()
+    #import ipdb; ipdb.set_trace()
     GameRunner.run()
 
 
