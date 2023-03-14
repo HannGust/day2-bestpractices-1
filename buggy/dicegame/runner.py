1b40cf31922eafe81764df0048ead959b40299a0
Hannes Gustafsson <hannes.gustafsson@kemi.uu.se>
2023-03-14 20:19:57 +0100

    Change comments and made printing nicer to the player, also removed import of the exception throwing function

--- buggy/dicegame/runner.py
+++ buggy/dicegame/runner.py
@@ -1,6 +1,5 @@
 from .die import Die
 from .die import roll
-from .utils import i_just_throw_an_exception
 
 class GameRunner:
 
@@ -24,42 +23,45 @@ class GameRunner:
 
     @classmethod
     def run(cls):
-        # Probably counts wins or something.
-        # Great variable name, 10/10.
+        # c: Counts consecutive wins.
         c = 0
         runner = cls() # BUGFIX 2: Moved instantiation to outside game loop, to avoid resetting rounds, wins, losses
         while True:
-            runner.reroll_dice() # BUGFIX 2: Reroll dice here instead of instatiating, to avoid resetting 
+            runner.reroll_dice() # BUGFIX 2: Reroll dice here instead of instantiating, to avoid resetting 
 
             print("Round {}\n".format(runner.round))
 
             for die in runner.dice:
                 print(die.show())
 
-            guess = input("Sigh. What is your guess?: ")
+            guess = input("What is your guess?: ")
             guess = int(guess)
 
             if guess == runner.answer():
-                print("Congrats, you can add like a 5 year old...")
+                print("Congrats, you guessed correctly!")
                 runner.wins += 1
                 c += 1
             else:
                 print("Sorry that's wrong")
                 print("The answer is: {}".format(runner.answer()))
-                print("Like seriously, how could you mess that up")
+                #print("Like seriously, how could you mess that up")
                 runner.loses += 1
                 c = 0
             print("Wins: {} Loses {}".format(runner.wins, runner.loses))
             runner.round += 1
 
             if c == 6:
-                print("You won... Congrats...")
-                print("The fact it took you so long is pretty sad")
+                print("You won! Well done! Congrats!")
+                #print("The fact it took you so long is pretty sad")
                 break
 
-            prompt = input("Would you like to play again?[Y/n]: ")
+            prompt = input("Would you like to play again?[(y)/n]: ") 
 
-            if prompt == 'y' or prompt == '':
+            if prompt != 'n' and prompt != 'N':  # FIX 3: Default is to continue now, you have to abort with n or N
                 continue
-            else:
-                i_just_throw_an_exception()
+            else: # FIX 3: Erased this exception throwing for a proper exit message
+                print('Okay, quiting the game.') 
+                print('You played {} rounds, and the final score was: '.format(runner.round))
+                print('Wins: {}, Losses: {} '.format(runner.wins, runner.loses))
+                print('You were on a streak of {} consecutive wins.'.format(c))
+                break
